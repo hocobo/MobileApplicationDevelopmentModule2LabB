c4178750e46795cab13af942ff04866e14ca1d4b
hocobo <79883046+hocobo@users.noreply.github.com>
2024-04-13 15:49:57 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Module2LabB
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Module2LabB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            mDatePicker.Date = new DateTime(2005, 1, 1);
+            mDatePicker.DateSelected += Calculate_Years;
+        }
+
+        private void Calculate_Years(object sender, DateChangedEventArgs e)
+        {
+            DateTime selectedDate = mDatePicker.Date;          
+            DateTime currentDate = DateTime.Today;
+            int yearsUntilLegalAge = 21 - (currentDate.Year - selectedDate.Year);
+            YearsUntilLegal.Text = $"Years until legal drinking age: {yearsUntilLegalAge}";
+        }
+    }
+}
